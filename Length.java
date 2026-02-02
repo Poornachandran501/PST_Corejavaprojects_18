2d8d0d661f9439bbc7263fbaefae2ae48ee162df
Poorna Chandran <cpoorna414@gmail.com>
2026-02-02 09:37:29 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,21 @@
+package Coreproject;
+import java.util.Scanner;
+public class Length {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            count++;
+        }
+  
+      System.out.println("Length of the string = " + count);
+
+        sc.close();
+	}
+}
